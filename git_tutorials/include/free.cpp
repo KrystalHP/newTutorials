a46a273e1acbd285e55f1190565b45200fb450ac
Krystal <krystal22.shpp@gmail.com>
2023-05-12 11:30:28 +0630

    add free.cpp

---
+++ git_tutorials/include/free.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int now = 0;
+int birth = 0;
+int age =0 ;
+
+cout<<"Please enter now: ";
+cin>>now;
+
+cout<<"Please enter your birth: ";
+cin>>birth;
+
+age = now-birth;
+
+cout<<"This is your age: "<<age;
+
+}
